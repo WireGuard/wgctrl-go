f2fc039bf3baa89f1c5093cc0a376046a3de3290
Matt Layher <mdlayher@gmail.com>
2018-07-21 14:07:07 -0400

    wireguardnl: initial Linux device fetching

--- client_linux.go
+++ client_linux.go
@@ -3,8 +3,15 @@
 package wireguardnl
 
 import (
+	"fmt"
+	"net"
+	"os"
+
 	"github.com/mdlayher/genetlink"
+	"github.com/mdlayher/netlink"
+	"github.com/mdlayher/netlink/nlenc"
 	"github.com/mdlayher/wireguardnl/internal/wgh"
+	"golang.org/x/sys/unix"
 )
 
 var _ osClient = &client{}
@@ -46,7 +53,31 @@ func (c *client) Close() error {
 
 // Devices implements osClient.
 func (c *client) Devices() ([]*Device, error) {
-	return nil, nil
+	// TODO(mdlayher): it doesn't seem possible to do a typical netlink dump
+	// of all WireGuard devices.  Perhaps consider raising this to the developers
+	// to solicit their feedback.
+	ifis, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	var ds []*Device
+	for _, ifi := range ifis {
+		// Attempt to fetch device information.  If we receive a "not exist"
+		// error, the device must not be a WireGuard device.
+		d, err := c.getDevice(ifi.Index, ifi.Name)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+
+			return nil, err
+		}
+
+		ds = append(ds, d)
+	}
+
+	return ds, nil
 }
 
 // DeviceByIndex implements osClient.
@@ -62,5 +93,82 @@ func (c *client) DeviceByName(name string) (*Device, error) {
 // getDevice fetches a Device using either its index or name, depending on which
 // is specified.  If both are specified, index is preferred.
 func (c *client) getDevice(index int, name string) (*Device, error) {
-	return nil, nil
+	// WireGuard netlink expects either interface index or name for all queries.
+	var attr netlink.Attribute
+	switch {
+	case index != 0:
+		attr = netlink.Attribute{
+			Type: wgh.DeviceAIfindex,
+			Data: nlenc.Uint32Bytes(uint32(index)),
+		}
+	case name != "":
+		attr = netlink.Attribute{
+			Type: wgh.DeviceAIfname,
+			Data: nlenc.Bytes(name),
+		}
+	default:
+		// No information provided, nothing to do.
+		return nil, os.ErrNotExist
+	}
+
+	b, err := netlink.MarshalAttributes([]netlink.Attribute{attr})
+	if err != nil {
+		return nil, err
+	}
+
+	msg := genetlink.Message{
+		Header: genetlink.Header{
+			Command: wgh.CmdGetDevice,
+			Version: wgh.GenlVersion,
+		},
+		Data: b,
+	}
+
+	flags := netlink.HeaderFlagsRequest | netlink.HeaderFlagsDump
+
+	msgs, err := c.c.Execute(msg, c.family.ID, flags)
+	if err != nil {
+		switch err {
+		// Convert "no such device" and "not a wireguard device" to an error
+		// compatible with os.IsNotExist for easy checking.
+		case unix.ENODEV, unix.ENOTSUP:
+			return nil, os.ErrNotExist
+		default:
+			return nil, err
+		}
+	}
+
+	if len(msgs) > 1 {
+		return nil, fmt.Errorf("wireguardnl: unexpected number of response messages: %d", len(msgs))
+	}
+
+	return parseDevice(msgs[0])
+}
+
+// parseDevice parses a Device from a generic netlink message.
+func parseDevice(m genetlink.Message) (*Device, error) {
+	attrs, err := netlink.UnmarshalAttributes(m.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	var d Device
+	for _, a := range attrs {
+		switch a.Type {
+		case wgh.DeviceAIfindex:
+			d.Index = int(nlenc.Uint32(a.Data))
+		case wgh.DeviceAIfname:
+			d.Name = nlenc.String(a.Data)
+		case wgh.DeviceAPrivateKey:
+			d.PrivateKey = newKey(a.Data)
+		case wgh.DeviceAPublicKey:
+			d.PublicKey = newKey(a.Data)
+		case wgh.DeviceAListenPort:
+			d.ListenPort = int(nlenc.Uint16(a.Data))
+		case wgh.DeviceAFwmark:
+			d.FirewallMark = int(nlenc.Uint32(a.Data))
+		}
+	}
+
+	return &d, nil
 }
