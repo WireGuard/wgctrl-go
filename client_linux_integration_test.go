55c2dba2ceed7a05daf05f3f739906bf6d62062b
Matt Layher <mdlayher@gmail.com>
2018-07-21 15:26:30 -0400

    wireguardnl: basic integration test

---
+++ client_linux_integration_test.go
@@ -0,0 +1,39 @@
+//+build linux
+
+package wireguardnl_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/mdlayher/wireguardnl"
+)
+
+func TestLinuxClientIntegration(t *testing.T) {
+	c, err := wireguardnl.New()
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Skip("skipping, wireguardnl is not available on this system")
+		}
+
+		t.Fatalf("failed to open client: %v", err)
+	}
+	defer c.Close()
+
+	// TODO(mdlayher): expand upon this.
+
+	t.Run("devices", func(t *testing.T) {
+		devices, err := c.Devices()
+		if err != nil {
+			if os.IsPermission(err) {
+				t.Skip("skipping, wireguardnl requires elevated privileges")
+			}
+
+			t.Fatalf("failed to get devices: %v", err)
+		}
+
+		for _, d := range devices {
+			t.Logf("device: %s: %s", d.Name, d.PublicKey.String())
+		}
+	})
+}
