4319862de972e35e6e7e95f8e9e22107b02aff60
Matt Layher <mdlayher@gmail.com>
2018-07-23 11:33:40 -0400

    wireguardnl: encourage use of wireguardctrl

--- wireguardnl/doc.go
+++ wireguardnl/doc.go
@@ -1,5 +1,9 @@
 // Package wireguardnl provides access to Linux's WireGuard generic netlink
 // interface.
 //
+// Users are encouraged to use the wireguardctrl package instead, as it provides
+// an abstracted interface that works on both Linux and non-Linux operating
+// systems.
+//
 // For more information on WireGuard, please see https://www.wireguard.com/.
 package wireguardnl
