c8161aea05217c8c7860f25b4d4858967ee0760f
Matt Layher <mdlayher@gmail.com>
2018-07-24 17:41:47 -0400

    internal/*: point users to package wireguardctrl

--- internal/wireguardcfg/doc.go
+++ internal/wireguardcfg/doc.go
@@ -1,9 +1,6 @@
-// Package wireguardcfg provides access to the userspace WireGuard configuration
-// protocol interface.
+// Package wireguardcfg provides internal access to the userspace WireGuard
+// configuration protocol interface.
 //
-// Users are encouraged to use the wireguardctrl package instead, as it provides
-// an abstracted interface that works on both Linux and non-Linux operating
-// systems.
-//
-// For more information on WireGuard, please see https://www.wireguard.com/.
+// This package is internal-only and not meant for end users to consume.
+// Please use package wireguardctrl (an abstraction over this package) instead.
 package wireguardcfg
