2cd792008a5dd2fb05fc886abc7eb8859e71ae43
Matt Layher <mdlayher@gmail.com>
2018-07-24 13:08:08 -0400

    internal/wireguardcfg: initial socket detection and test

--- internal/wireguardcfg/client.go
+++ internal/wireguardcfg/client.go
@@ -1,35 +1,120 @@
 package wireguardcfg
 
 import (
-	"errors"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/mdlayher/wireguardctrl/wgtypes"
 )
 
-var (
-	errUnimplemented = errors.New("wireguardcfg: userspace configuration protocol not yet implemented")
-)
-
 // A Client provides access to userspace WireGuard device information.
-type Client struct{}
+type Client struct {
+	findSockets func() ([]string, error)
+}
 
 // New creates a new Client.
-func New() (*Client, error) { return nil, errUnimplemented }
+func New() (*Client, error) {
+	return &Client{
+		findSockets: func() ([]string, error) {
+			return findSocketFiles([]string{
+				// TODO(mdlayher): deal with /var/run symlink?
+				"/run/wireguard",
+			})
+		},
+	}, nil
+}
 
 // Close releases resources used by a Client.
-func (c *Client) Close() error { return errUnimplemented }
+func (c *Client) Close() error { return nil }
 
 // Devices retrieves all WireGuard devices on this system.
-func (c *Client) Devices() ([]*wgtypes.Device, error) { return nil, errUnimplemented }
+func (c *Client) Devices() ([]*wgtypes.Device, error) {
+	socks, err := c.findSockets()
+	if err != nil {
+		return nil, err
+	}
+
+	var ds []*wgtypes.Device
+	for _, sock := range socks {
+		d, err := getDevice(sock)
+		if err != nil {
+			return nil, err
+		}
+
+		ds = append(ds, d)
+	}
+
+	return ds, nil
+}
 
 // DeviceByIndex retrieves a WireGuard device by its interface index.
 //
 // If the device specified by index does not exist or is not a WireGuard device,
 // an error is returned which can be checked using os.IsNotExist.
-func (c *Client) DeviceByIndex(_ int) (*wgtypes.Device, error) { return nil, errUnimplemented }
+func (c *Client) DeviceByIndex(index int) (*wgtypes.Device, error) {
+	ifi, err := net.InterfaceByIndex(index)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.DeviceByName(ifi.Name)
+}
 
 // DeviceByName retrieves a WireGuard device by its interface name.
 //
 // If the device specified by name does not exist or is not a WireGuard device,
 // an error is returned which can be checked using os.IsNotExist.
-func (c *Client) DeviceByName(_ string) (*wgtypes.Device, error) { return nil, errUnimplemented }
+func (c *Client) DeviceByName(name string) (*wgtypes.Device, error) {
+	socks, err := c.findSockets()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sock := range socks {
+		if name != deviceName(sock) {
+			continue
+		}
+
+		return getDevice(sock)
+	}
+
+	return nil, os.ErrNotExist
+}
+
+// deviceName infers a device name from an absolute file path with extension.
+func deviceName(sock string) string {
+	return strings.TrimSuffix(filepath.Base(sock), filepath.Ext(sock))
+}
+
+// findSocketFiles looks for UNIX socket files in the specified directories.
+func findSocketFiles(dirs []string) ([]string, error) {
+	var socks []string
+	for _, d := range dirs {
+		files, err := ioutil.ReadDir(d)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+
+			return nil, err
+		}
+
+		for _, f := range files {
+			if f.Mode()&os.ModeSocket == 0 {
+				continue
+			}
+
+			socks = append(socks, filepath.Join(d, f.Name()))
+		}
+	}
+
+	return socks, nil
+}
+
+// getDevice retrieves a Device using its socket file.
+func getDevice(path string) (*wgtypes.Device, error) {
+	return nil, nil
+}
---
+++ internal/wireguardcfg/client_test.go
@@ -0,0 +1,49 @@
+package wireguardcfg
+
+import (
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_findSocketFiles(t *testing.T) {
+	tmp, err := ioutil.TempDir(os.TempDir(), "wireguardcfg-test")
+	if err != nil {
+		t.Fatalf("failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(tmp)
+
+	// Create a file which is not a device socket.
+	f, err := ioutil.TempFile(tmp, "notwg")
+	if err != nil {
+		t.Fatalf("failed to create temporary file: %v", err)
+	}
+	_ = f.Close()
+
+	// Create a temporary UNIX socket and leave it open so it is picked up
+	// as a socket file.
+	path := filepath.Join(tmp, "testwg0.sock")
+	l, err := net.Listen("unix", path)
+	if err != nil {
+		t.Fatalf("failed to create socket: %v", err)
+	}
+	defer l.Close()
+
+	files, err := findSocketFiles([]string{
+		tmp,
+		// Should gracefully handle non-existant directories and files.
+		filepath.Join(tmp, "foo"),
+		"/not/exist",
+	})
+	if err != nil {
+		t.Fatalf("failed to find files: %v", err)
+	}
+
+	if diff := cmp.Diff([]string{path}, files); diff != "" {
+		t.Fatalf("unexpected output files (-want +got):\n%s", diff)
+	}
+}
