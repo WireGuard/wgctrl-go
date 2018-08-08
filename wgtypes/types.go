d21ae4f820d0c9d6b524b181ae3e5a5f58dba797
Matt Layher <mdlayher@gmail.com>
2018-08-07 23:08:26 -0400

    wgtypes: first pass at Device and Peer documentation

--- wgtypes/types.go
+++ wgtypes/types.go
@@ -14,12 +14,26 @@ import (
 
 // A Device is a WireGuard device.
 type Device struct {
-	Name         string
-	PrivateKey   Key
-	PublicKey    Key
-	ListenPort   int
+	// Name is the name of the device.
+	Name string
+
+	// PrivateKey is the device's private key.
+	PrivateKey Key
+
+	// PublicKey is the device's public key, computed from its PrivateKey.
+	PublicKey Key
+
+	// ListenPort is the device's network listening port.
+	ListenPort int
+
+	// FirewallMark is the device's current firewall mark.
+	//
+	// The firewall mark can be used in conjunction with firewall software to
+	// take action on outgoing WireGuard packets.
 	FirewallMark int
-	Peers        []Peer
+
+	// Peers is the list of network peers associated with this device.
+	Peers []Peer
 }
 
 const keyLen = 32 // wgh.KeyLen
@@ -102,14 +116,46 @@ func (k Key) String() string {
 
 // A Peer is a WireGuard peer to a Device.
 type Peer struct {
-	PublicKey                   Key
-	PresharedKey                Key
-	Endpoint                    *net.UDPAddr
+	// PublicKey is the public key of a peer, computed from its private key.
+	//
+	// PublicKey is always present in a Peer.
+	PublicKey Key
+
+	// PresharedKey is an optional preshared key which may be used as an
+	// additional layer of security for peer communications.
+	//
+	// A zero-value Key means no preshared key is configured.
+	PresharedKey Key
+
+	// Endpoint is the most recent source address used for communication by
+	// this Peer.
+	Endpoint *net.UDPAddr
+
+	// PersistentKeepaliveInterval specifies how often an "empty" packet is sent
+	// to a peer to keep a connection alive.
+	//
+	// A value of 0 indicates that persistent keepalives are disabled.
 	PersistentKeepaliveInterval time.Duration
-	LastHandshakeTime           time.Time
-	ReceiveBytes                int64
-	TransmitBytes               int64
-	AllowedIPs                  []net.IPNet
+
+	// LastHandshakeTime indicates the most recent time a handshake was performed
+	// with this peer.
+	//
+	// A zero-value time.Time indicates that no handshake has taken place with
+	// this peer.
+	LastHandshakeTime time.Time
+
+	// ReceiveBytes indicates the number of bytes received from this peer.
+	ReceiveBytes int64
+
+	// TransmitBytes indicates the number of bytes transmitted to this peer.
+	TransmitBytes int64
+
+	// AllowedIPs specifies which IPv4 and IPv6 addresses this peer is allowed
+	// to communicate on.
+	//
+	// 0.0.0.0/0 indicates that all IPv4 addresses are allowed, and ::/0
+	// indicates that all IPv6 addresses are allowed.
+	AllowedIPs []net.IPNet
 }
 
 // A Config is a WireGuard device configuration.
