442c5ecc17be7894bc6c4f63fefd196ec52c20e5
Matt Layher <mdlayher@gmail.com>
2019-05-16 20:50:16 -0400

    internal/wgopenbsd: implement peer parsing

--- internal/wgopenbsd/internal/wgh/defs.go
+++ internal/wgopenbsd/internal/wgh/defs.go
@@ -8,6 +8,7 @@ package wgh
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/sockio.h>
+#include <time.h>
 #include "if_wg.h"
 */
 import "C"
@@ -20,8 +21,21 @@ type Ifgroupreq C.struct_ifgroupreq
 
 type Ifgreq C.struct_ifg_req
 
+type Timespec C.struct_timespec
+
 // WireGuard types and constants.
 
-const SIOCGWGSERV = C.SIOCGWGSERV
+const (
+	SIOCGWGSERV = C.SIOCGWGSERV
+	SIOCGWGPEER = C.SIOCGWGPEER
+
+	WGStateNoSession = C.WG_STATE_NO_SESSION
+	WGStateInitiator = C.WG_STATE_INITIATOR
+	WGStateResponder = C.WG_STATE_RESPONDER
+)
 
 type WGGetServ C.struct_wg_get_serv
+
+type WGGetPeer C.struct_wg_get_peer
+
+type WGIP C.union_wg_ip
